94e61547c1451d10019e502dba0a4b7d1f1d80b9
Phatnguyen27 <43376008+Phatnguyen27@users.noreply.github.com>
2019-11-06 16:29:30 +0700

    gmailverification

--- app/src/main/java/com/example/nowdiary/RegisterActivity.java
+++ app/src/main/java/com/example/nowdiary/RegisterActivity.java
@@ -14,6 +14,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RegisterActivity  extends AppCompatActivity {
     private Button mOKButton,mCancelButton;
@@ -40,24 +45,32 @@ public class RegisterActivity  extends AppCompatActivity {
         mOKButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final String username = mUsername.getText().toString();
+                final String password = mPassword.getText().toString();
                 myProgress = new ProgressDialog(RegisterActivity.this);
                 myProgress.setTitle("Authenticatiing");
-                myProgress.setMessage("Please wait...");
+                myProgress.setMessage("Please wait ...");
                 myProgress.setCancelable(true);
                 myProgress.show();
-                String username = mUsername.getText().toString();
-                String password = mPassword.getText().toString();
                 String validation = mValidation.getText().toString();
                 if(checkLength(username) && checkLength(password) && checkLength(validation)) {
                     if(password.equals(validation)) {
                         auth.createUserWithEmailAndPassword(username,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-                                myProgress.dismiss();
                                 if(task.isSuccessful()) {
-                                    Toast.makeText(RegisterActivity.this,"Account succesfully created",Toast.LENGTH_LONG).show();
-                                    finish();
-
+                                    createUserNode(auth.getCurrentUser().getUid(),password);
+                                    auth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()) {
+                                                myProgress.dismiss();
+                                                String status = "Register Successfully. Please check your email for verification!";
+                                                Toast.makeText(RegisterActivity.this, status, Toast.LENGTH_SHORT).show();
+                                                finish();
+                                            }
+                                        }
+                                    });
                                 }
                             }
                         });
@@ -76,5 +89,33 @@ public class RegisterActivity  extends AppCompatActivity {
 
             }
         });
+    }
+    public void createUserNode(final String userId, final String nickname) {
+        final DatabaseReference mDbRoot = FirebaseDatabase.getInstance().getReference();
+        mDbRoot.child(userId)
+                .addListenerForSingleValueEvent(
+                        new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if (!dataSnapshot.exists()) {
+                                    mDbRoot.child(userId).child("userInfo").child("username").setValue(nickname)
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    if (task.isSuccessful()) {
+                                                    } else {
+                                                        Toast.makeText(RegisterActivity.this, "Wrong", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                }
+                                            });
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        }
+                );
     }
 }
